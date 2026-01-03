71d1c6e67366cffba23ce899785e153143c966ff
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2026-01-03 12:02:52 -0500

    create simple textbuffer vec

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{Column, text};
+use iced::{Element, Settings};
+
+fn main() -> iced::Result {
+    iced::run(Icarus::update, Icarus::view)
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    ButtonPressed,
+}
+
+#[derive(Default)]
+struct TextBuffer {
+    lines: Vec<String>,
+}
+
+impl TextBuffer {
+    fn new() -> Self {
+        let lines = vec!["Hello".to_string(), "world".to_string()];
+        Self { lines }
+    }
+
+    fn to_column(&self) -> Column<'_, Message> {
+        let elements: Vec<Element<'_, Message>> =
+            self.lines.iter().map(|s| text(s).into()).collect();
+        Column::with_children(elements)
+    }
+}
+
+struct Icarus {
+    buffer: TextBuffer,
+}
+
+impl Default for Icarus {
+    fn default() -> Self {
+        Self {
+            buffer: TextBuffer::new(),
+        }
+    }
+}
+
+impl Icarus {
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::ButtonPressed => todo!(),
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        self.buffer.to_column().into()
+    }
 }
